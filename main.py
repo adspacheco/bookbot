2a9ec534df4ad5dc68ee4860a941a5c0199c109c
adspacheco <andersonac7@outlook.com>
2024-11-03 20:38:30 -0300

    print report

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
-    file_path = 'books/frankenstein.txt'
+    file_path = "books/frankenstein.txt"
+    
     data = read_file(file_path)
+    
     words_count = count_words(data)
     chars_count = count_characters(data)
-    print(f'Total words in file: {words_count}')
-    print(f'Characters count: {chars_count}')
+    sorted_chars_count = sort_dict(chars_count)
+    
+    print_report(file_path, words_count, sorted_chars_count)
+
+
 def read_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return file.read()
 
 def count_words(data):
@@ -16,11 +21,26 @@ def count_words(data):
 def count_characters(data):
     counter = {}
     for char in data.lower():
-        if char.isalnum():
+        if char.isalpha():
             if char in counter:
                 counter[char] += 1
             else:
                 counter[char] = 1
     return counter
+
+def sort_dict(dict):
+    sorted_list = sorted(dict.items(), key=lambda item: item[1], reverse=True)
+    return sorted_list
+
+def print_report(file_path, words_count, sorted_chars_count):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{words_count} words found in the document")
+    print("Characters found in the document:")
+    print("------------------------------------")
+    for char, count in sorted_chars_count:
+        print(f"The caracter '{char}' was found {count} times")
+
+    print("--- End report ---")
+
 
 main()
